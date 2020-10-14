fe16c6ed040b352285ad19972aa32f49bb9d3313
richa031 <richahp031@gmail.com>
2020-10-14 20:19:03 +0530

    adding 404 Error

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 #take all the requests and sends back an http response
+from django.http import Http404 
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -13,6 +14,10 @@ def index(request):
     return render(request, 'music/index.html', context) #shortcut to render template
 
 def detail(request, id):
-    return HttpResponse("<h2>Details for Album id: " + str(id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
 
 
