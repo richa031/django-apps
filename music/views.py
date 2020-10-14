df82705b60a593cc14718b9c038056d2e1402490
richa031 <richahp031@gmail.com>
2020-10-14 12:46:29 +0530

    template render 2 shortcut

--- music/views.py
+++ music/views.py
@@ -3,17 +3,14 @@ from django.shortcuts import render
 #take all the requests and sends back an http response
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader #seperating html from main code
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all(); #connecting to db to access all the objects stored
-    template = loader.get_template('music/index.html') #django in default view will look into this file no need to write complete address
-    context = {#we pass the album info using this dict to our template
-        'all_albums': all_albums, 
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context) #shortcut to render template
 
 def detail(request, id):
     return HttpResponse("<h2>Details for Album id: " + str(id) + "</h2>")
