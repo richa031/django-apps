beda75ed43d27b41a7b4e78b8b01c6592d2a9c88
richa031 <richahp031@gmail.com>
2020-10-15 00:07:50 +0530

    adding form

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views #look for views.py in the same directory
 
+app_name = 'music' #namespacing urls 
+
 urlpatterns = [
     #/music/
     path('', views.index, name='index'), #index is the home page of the section in views.py
+    #/music/<album_id>/
     path('<int:id>/', views.detail, name='detail'), #name is used to remove hardcoded urls
+    #music/<album_id>/favorite/
+    path('<int:id>/favorite/', views.favorite, name='favorite'), #some urls return a functionality like login 
 ]
--- music/views.py
+++ music/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
-
 #take all the requests and sends back an http response
-from django.http import Http404 
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404 
+from .models import Album, Song
 
 
 def index(request):
@@ -14,10 +10,24 @@ def index(request):
     return render(request, 'music/index.html', context) #shortcut to render template
 
 def detail(request, id):
-    try:
-        album = Album.objects.get(pk=id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    #album = Album.objects.get(pk=id)
+    album = get_object_or_404(Album, pk=id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, id):
+    album = get_object_or_404(Album, pk=id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+        
 
 
